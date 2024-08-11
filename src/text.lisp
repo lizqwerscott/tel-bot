ed9b0b24bcade8bafbb9d30c178278a9881ca6ed
lizqwerscott <lizqwerscott@163.com>
2024-08-11 15:01:16 +0800

    fix: remove ybanapi and fix vvhan api

--- src/text.lisp
+++ src/text.lisp
@@ -36,9 +36,9 @@
 (defun wanan ()
   (get-zw "wanan"))
 
-;; joke sao love
+;; joke sexy love
 (defun get-random-text (command)
-  (web-get "api.vvhan.com" (format nil "api/~A" command)))
+  (web-get "api.vvhan.com" (format nil "api/text/~A" command)))
 
 (defun holidays ()
   (car
@@ -68,65 +68,32 @@
                      ("storytype" . ,story-type))
              :jsonp t))))
 
-(defvar *ybanapi* "ybapi.cn")
-
-(defun parse-text-show (text)
-  "\\n \r 正确解析"
-  (format nil (replace-all-l "\\r" "~%~%%" (replace-all-l "\\\n" "~%" text))))
-
-(defun get-fd-text (name)
-  "name: 发颠的对象"
-  (parse-text-show
-   (web-get *ybanapi*
-            "API/fd.php"
-            :args `(("name" . ,name)))))
-
-(defun get-cp-text (cp1 cp0)
-  "cp1: 攻, cp0: 受"
-  (parse-text-show
-   (web-get *ybanapi*
-            "API/cp.php"
-            :args `(("cp1" . ,cp1)
-                    ("cp0" . ,cp0)))))
-
-(defun get-acgyiyan ()
-  "输出 acg 名言"
-  (parse-text-show
-   (web-get *ybanapi*
-            "API/acgyiyan.php")))
-
-(defun get-kfc ()
-  "输出 kfc 文案"
-  (parse-text-show
-   (web-get *ybanapi*
-            "API/kfc.php")))
-
 (defcommand
     (:love "发送一段情话" chat text)
     (declare (ignorable text))
-  (handler-case
-      (let ((text (get-random-text "love")))
-        (reply text))
-    (error (c)
-      (reply (format nil "[Error]: ~A" c)))))
+    (handler-case
+        (let ((text (get-random-text "love")))
+          (reply text))
+      (error (c)
+        (reply (format nil "[Error]: ~A" c)))))
 
 (defcommand
-    (:sao "发送一段骚话" chat text)
+    (:sexy "发送一段骚话" chat text)
     (declare (ignorable text))
     (handler-case
-      (let ((text (get-random-text "sao")))
-        (reply text))
-    (error (c)
-      (reply (format nil "[Error]: ~A" c)))))
+        (let ((text (get-random-text "sexy")))
+          (reply text))
+      (error (c)
+        (reply (format nil "[Error]: ~A" c)))))
 
 (defcommand
     (:joke "发送一段笑话" chat text)
     (declare (ignorable text))
-  (handler-case
-      (let ((text (get-random-text "joke")))
-        (reply text))
-    (error (c)
-      (reply (format nil "[Error]: ~A" c)))))
+    (handler-case
+        (let ((text (get-random-text "joke")))
+          (reply text))
+      (error (c)
+        (reply (format nil "[Error]: ~A" c)))))
 
 (defun handle-story (story)
   (format nil
@@ -156,47 +123,6 @@
                    "只支持成语、睡前、童话、寓言"
                    (handle-story
                     (get-story :story-type match-res))))))
-      (error (c)
-        (reply (format nil "[Error]: ~A" c)))))
-
-(defcommand
-    (:fd "发颠(参数: 指定发颠名字)" chat text)
-    (handler-case
-        (reply
-         (if (string= text "")
-             "请输入发颠的对象"
-             (get-fd-text text)))
-      (error (c)
-        (reply (format nil "[Error]: ~A" c)))))
-
-(defcommand
-    (:acgyiyan "返回 ACG 名言" chat text)
-    (declare (ignorable text))
-    (handler-case
-        (reply
-         (get-acgyiyan))
-      (error (c)
-        (reply (format nil "[Error]: ~A" c)))))
-
-(defcommand
-    (:kfc "返回 Kfc 疯狂星期四文案" chat text)
-    (declare (ignorable text))
-    (handler-case
-        (reply
-         (get-kfc))
-      (error (c)
-        (reply (format nil "[Error]: ~A" c)))))
-
-(defcommand
-    (:cp "返回 Cp 文案: 参数(第一个参数: 攻, 第二个参数: 受)" chat text)
-    (handler-case
-        (reply
-         (let ((temp (split-s text)))
-           (let ((cp1 (first temp))
-                 (cp0 (second temp)))
-             (if (and cp1 cp0)
-                 (get-cp-text cp1 cp0)
-                 "参数不正确: 第一个参数: 攻, 第二个参数: 受"))))
       (error (c)
         (reply (format nil "[Error]: ~A" c)))))
 
